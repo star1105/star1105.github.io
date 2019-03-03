140fcb4293cd492c3c9afaaa82588f889cec51d7
star <1327678113@qq.com>
2019-03-03 14:24:52 +0800

    Update homework1/Yike/api/older.php

---
+++ homework1/Yike/api/older.php
@@ -0,0 +1,23 @@
+<?php
+	// 获得当前时间
+	// echo time();
+
+	// echo '<br>';
+
+	// echo date('Y-m-d', time());
+	
+	// echo '<br>';
+	// php 获得时间,获得前一天
+	$today = strtotime('-1day', time());
+
+	// 
+	$older = date('Y-m-d', $today);
+
+	// echo date('Y-m-d', $today);
+	// exit;
+	// 
+	$url = 'https://moment.douban.com/api/stream/date/'.$older.'?alt=json&apikey=0bcf52793711959c236df76ba534c0d4&app_version=1.7.4&douban_udid=d623045db9fcb0d5243174c1bf1a675f887047c0&format=full&udid=9a34d8b038ff38971050199b0c5ee9c60c6d1ca3&version=6';
+
+	echo file_get_contents($url);
+
+
